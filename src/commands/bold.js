28fae6ab1f4412954bcc113a65c9f7bb213a0e22
Shlomi Nissan <shlomi@betamark.com>
2021-01-05 16:38:46 -0800

    Add command toggling to the editor

--- src/commands/bold.js
+++ src/commands/bold.js
@@ -8,16 +8,33 @@ const Bold = function () {};
  * @param {HTMLDivElement} editor - contentEditable div tag.
  * @param {Object} textState - { text: string, position: number }
  */
-Bold.prototype.execute = function (editor, textState) {
-    const wordOffset = getSurroundingWord(textState.text, textState.position);
-    const word = setSelection(wordOffset);
-    const text = `**${word}**`;
+Bold.prototype.execute = function (editor, state) {
+    const regex = /(\*{2})(.*?)(\1)/g;
+    const wordOffset = getSurroundingWord(state.text, state.position);
 
+    let text = "";
+    let selection = { start: 0, end: 0 };
+
+    const addMarkdown = () => {
+        text = `**${word}**`;
+        selection = {
+            start: wordOffset.start + 2,
+            end: wordOffset.end + 2,
+        };
+    };
+
+    const stripMarkdown = () => {
+        text = word.replace(regex, "$2");
+        selection = {
+            start: state.position - 2,
+            end: state.position - 2,
+        };
+    };
+
+    const word = setSelection(wordOffset);
+    word.match(regex) ? stripMarkdown() : addMarkdown();
     insertText(editor, text);
-    setSelection({
-        start: wordOffset.start + 2,
-        end: wordOffset.end + 2,
-    });
+    setSelection(selection);
 };
 
 const Instance = new Bold();
--- src/commands/header.js
+++ src/commands/header.js
@@ -6,16 +6,31 @@ const Header = function () {};
 /**
  * Add header markdown to current paragraph.
  * @param {HTMLDivElement} editor - contentEditable div tag.
- * @param {Object} textState - { text: string, position: number }
+ * @param {Object} state - { text: string, position: number }
  * @param {number} size - 1, 2, ... 6
  */
-Header.prototype.execute = function (editor, textState, size) {
-    const hash = "#";
-    const text = `${hash.repeat(size)} ${textState.text}`;
+Header.prototype.execute = function (editor, state, size) {
+    const regex = new RegExp(`^(\\#{${size}}\\s)(.*?)`, "g");
+    const offset = size + 1;
 
-    setSelection({ start: 0, end: textState.text.length });
+    let text = "";
+    let cursor = { start: 0, end: 0 };
+
+    const addMarkdown = () => {
+        text = `${"#".repeat(size)} ${state.text}`;
+        cursor = state.position + offset;
+        setSelection({ start: 0, end: state.text.length });
+    };
+
+    const stripMarkdown = () => {
+        text = state.text.substring(size + 1, state.text.length);
+        cursor = state.position - offset;
+        setSelection({ start: 0, end: state.text.length });
+    };
+
+    state.text.match(regex) ? stripMarkdown() : addMarkdown();
     insertText(editor, text);
-    setSelection({ start: text.length, end: text.length });
+    setSelection({ start: cursor, end: cursor });
 };
 
 const Instance = new Header();
